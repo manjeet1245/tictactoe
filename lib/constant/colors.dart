a88ada55bae3ea983261302b140f3499d38b6009
Manjeet Singh <manjeets1245@gmail.com>
2023-07-03 20:57:12 +0530

    final commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Colors.pink;
+  static Color secondaryColor = Colors.yellow;
+  static Color accentColor = Color.fromARGB(255, 30, 80, 121);
+}
